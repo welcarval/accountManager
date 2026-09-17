4202100855889bd892d9e31bc3f8e74b249a5b97
Welton Carvalho <welton.carvalho.dev@gmail.com>
2026-09-17 17:29:43 -0300

    feat: add more fields to Account

--- src/main/java/dev/arcturuz/accountmanager/entities/Account.java
+++ src/main/java/dev/arcturuz/accountmanager/entities/Account.java
@@ -2,12 +2,21 @@ package dev.arcturuz.accountmanager.entities;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @Entity
 public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private Integer number;
+
+    private BigDecimal balance;
+
+    private LocalDate creationDate;
 
     @OneToOne
     @JoinColumn(name = "id")
