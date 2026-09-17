12057addc806876a5a473bb80760d3e88b95e76d
Welton Carvalho <welton.carvalho.dev@gmail.com>
2026-09-17 18:43:39 -0300

    fix: remove nonExisting User fields mapping

--- src/main/java/dev/arcturuz/accountmanager/mappers/UserMapper.java
+++ src/main/java/dev/arcturuz/accountmanager/mappers/UserMapper.java
@@ -3,12 +3,10 @@ package dev.arcturuz.accountmanager.mappers;
 import dev.arcturuz.accountmanager.dto.UserDTO;
 import dev.arcturuz.accountmanager.entities.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(target = "account", ignore = true)
     UserDTO toDTO(User entity);
 
     User toEntity(UserDTO dto);
